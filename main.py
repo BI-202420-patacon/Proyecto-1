cb4c5a972c9ffff6bbedc137acc3fe5f1890657e
JorgeB <j.bustamantep@uniandes.edu.co>
2024-10-09 10:48:21 -0500

    Predicción de texto funciona

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from joblib import load
 import pandas as pd
 from preprocesamiento import prep 
@@ -18,10 +18,18 @@ def read_root():
 def read_item(item_id: int, q: Optional[str] = None):
    return {"item_id": item_id, "q": q}
 
-@app.post("/predict")
+@app.post("/predictText")
 def make_predictions(dataModel: DataModel):
     df = pd.DataFrame(dataModel.dict(), columns=dataModel.dict().keys(), index=[0])
     df.columns = dataModel.columns()
+    model = load("modelo_Proyecto1.joblib")
+    result = model.predict(df["texto"])
+    return {"prediction": result.tolist()[0]}
+
+@app.post("/predictCSV")
+def make_predictions_csv(file: UploadFile = File(...)):
+    df = pd.read_csv(file.file)
+    df = prep(df)
     model = load("modelo_Proyecto1.joblib")
     result = model.predict(df)
     return {"prediction": result.tolist()}
